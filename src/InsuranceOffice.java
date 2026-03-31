cca696d91bd8adde458dc46f3e49c0181104ac67
Kerem Özüak <s35175@pjwstk.edu.pl>
2026-03-31 11:15:37 +0200

    Task 3: Add InsuranceOffice class

---
+++ src/InsuranceOffice.java
@@ -0,0 +1,24 @@
+﻿package src;
+
+import java.util.ArrayList;
+
+public class InsuranceOffice {
+    private String officeName;
+    private ArrayList<Policy> policies; 
+    
+    public InsuranceOffice(String officeName) {
+        this.officeName = officeName;
+        this.policies = new ArrayList<>();
+    }
+    
+    public void addPolicy(Policy policy) {
+        policies.add(policy);
+    }
+    
+    public void displayAllPolicies() {
+        System.out.println("--- Policies in " + officeName + " ---");
+        for (Policy p : policies) { 
+            System.out.println(p.toString()); 
+        }
+    }
+}
